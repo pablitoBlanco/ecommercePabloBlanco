2fa60ce23f4aeb61923c6f3b857a8e395d57f03c
Pablo Blanco <pabloblanco_@hotmail.com>
2021-10-12 22:01:07 -0300

    creacion navBar

---
+++ src/components/navBar.js
@@ -0,0 +1,16 @@
+export const NavBar = () => {
+  return(
+  <div className="menu">
+    <div className="holder">
+      <p className="falsoLogo">Hexagon Discos</p>
+      <nav>
+        <ul className="lista">
+          <li>inicio</li>
+          <li>generos</li>
+          <li>contacto</li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  )
+}
