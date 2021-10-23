c8c0683d8a65b9d6d78ae1741ad3d1306de49b34
Pablo Blanco <pabloblanco_@hotmail.com>
2021-10-23 12:30:49 -0300

    cambio ubicacion arrayDiscos

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,12 +1,8 @@
 import {Item} from "../Item/Item.js"
 import {useState, useEffect} from "react"
+import {arrayDiscos} from "../Item/arrayDiscos.js"
 
 
-
-const arrayDiscos = [{ id: 1, name: "MHTRT", band: "Boards of Canada", price: 3000, photo: "https://img.discogs.com/nyWy7vnrmdXV0rcyPrd7vhrgHlQ=/fit-in/600x595/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-11778-1475170215-9229.jpeg.jpg" },
-{ id: 2, name: "Aenima", band: "TOOL", price: 2500, photo: "https://www.nacionrock.com/wp-content/uploads/aenima.jpg" },
-{ id: 3, name: "El baile aleman", band: "Señor coconut", price: 3500, photo: "https://img.discogs.com/dp98D1xp1FwvppmCdw9BTEHw2XY=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-55427-1337875009-2157.jpeg.jpg" },
-{ id: 4, name: "Man made object", band: "Gogo penguin", price: 2700, photo: "https://img.discogs.com/D8JTyghBLIhOgwPfAsIv4XZ12is=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-8062430-1454420247-1268.jpeg.jpg" }]
 
 
  export const ItemList = () => {
